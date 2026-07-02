cf41ee7817909e2c889bb0e5f7f50a02a1ad1804
Vedant_Karkar <vedantkarkar091@gmail.com>
2026-07-02 16:21:48 +0530

    Updated candidate_selection.cpp with another version that calculates the probablity of best hired candidate

--- candidate_selection.cpp
+++ candidate_selection.cpp
@@ -1,28 +1,6 @@
-// 1. 
-// in strategy_for() : 
-// vector<double> candidates(100);
-
-// 2. 
-// Instead of double strategy_for(...)
-// introduce
-// struct Candidate
-// {
-//     double skill;
-// };
-// Later:
-// struct HiringResult
-// {
-//     double hired_skill;
-//     double best_skill;
-//     bool hired_best;
-//     int hired_rank;
-//     int hired_index;
-// };
-
 #include <iostream> 
 #include <random> 
 #include <vector>
-#include <numeric> // for std::accumulate()
 #include <map>
 #include <iomanip>
 
@@ -42,33 +20,86 @@ class Secretary {
         return skill_dist(rng); 
     }
 
-    // rejection can be from 0 meaning take first candidate, to 99 meaning take the last candidate
-    double strategy_for(int rejection) {
-        double max_skill = 0.0;
+    struct HiringResult {
+        double best_skill;
+        double hired_skill;
+        int hired_index;
+        int hired_rank;
+        bool hired_best;
+    };
 
-        if(rejection != 0) {
-            max_skill = generate_skill(); 
+    struct Printer {
+        int rejections; 
+        double final_avg_skill;
+        double final_prob;
+        double final_avg_rank;
+    };
+
+    double best_skilled(vector<double>& candidate) {
+        double local_max = candidate.at(0);
+        for(int i = 1; i < candidate.size(); i++) {
+            if(candidate.at(i) > local_max){
+                local_max = candidate.at(i);
+            }
+        }
+        return local_max;
+    }
+
+    int hired_candidate_index(vector<double>& candidate, int rejection) {
+
+        if(rejection == 0) { 
+            return 0; 
         }
 
+        double rejected_max_skill = candidate.at(0);
+
         for(int i = 1; i < rejection; i++) {
-            double candidate = generate_skill();
-            if(candidate > max_skill) {
-                max_skill = candidate;
+            if(candidate.at(i) > rejected_max_skill) {
+                rejected_max_skill = candidate.at(i);
             }
         }
 
-        for(int i = rejection; i < (total-1); i++) {
-            double candidate = generate_skill(); 
-            if(candidate > max_skill) {
-                return candidate;
+        for(int i = rejection; i < (total-1); i++) { 
+            if(candidate.at(i) > rejected_max_skill) {
+                return i;
             }
         }
         
-        return generate_skill();
+        return (total - 1);
     }
 
-    vector<double> generate_data_for(int rejection, int trials) {
-        vector<double> data; 
+    int hired_rank(const vector<double>& candidate, double hired_man) {
+        int rank = 1; 
+        for(int i = 0; i < candidate.size(); i++) {
+            if(candidate.at(i) > hired_man) {
+                rank++;
+            }
+        }
+        return rank;
+    }
+
+    // rejection can be from 0 meaning take first candidate, to 99 meaning take the last candidate
+    HiringResult strategy_for(int rejection) {
+        vector<double>candidate;
+        candidate.reserve(total);
+
+        for(int i = 0; i < total; i++) {
+            candidate.push_back(generate_skill());
+        }
+
+        HiringResult hired_emp;
+
+        hired_emp.best_skill = best_skilled(candidate);
+        hired_emp.hired_index = hired_candidate_index(candidate, rejection);
+        hired_emp.hired_skill = candidate.at(hired_emp.hired_index);
+        hired_emp.hired_rank = hired_rank(candidate, hired_emp.hired_skill); 
+        hired_emp.hired_best = (hired_emp.hired_rank == 1);
+
+        return hired_emp;
+    }
+
+    vector<HiringResult> generate_data_for(int rejection, int trials) {
+        vector<HiringResult> data; 
         data.reserve(trials);
         for(int i = 0; i < trials; i++) {
             data.push_back(strategy_for(rejection));
@@ -76,74 +107,143 @@ class Secretary {
         return data;
     }
 
-    double average_skill(const vector<double>& data) {
+    double average_skill(const vector<HiringResult>& data) {
         if(data.empty()) {
             return 0.0;
         }
-        double sum = (accumulate(data.begin(), data.end(), 0.0));
+        double sum = 0.0;
+        for(int i = 0; i < data.size(); i++) {
+            sum += (data.at(i)).hired_skill;
+        }
         return sum/data.size();
     }
 
-    map<int, double> perform_experiment(int trials) {
+    double prob_hired_best(const vector<HiringResult>& data){
+        if(data.empty()) {
+            return 0.0;
+        }
+        double success = 0;
+        for(int i = 0; i < data.size(); i++) {
+            if((data.at(i)).hired_best) {
+                success++;
+            }
+        }
+        return success/data.size();
+    }
+
+    double average_rank(const vector<HiringResult>& data) {
+        if(data.empty()) {
+            return -1;
+        }
+        double sum = 0.0;
+        for(int i = 0; i < data.size(); i++) {
+            sum += (data.at(i)).hired_rank;
+        }
+        return sum/data.size();
+    }
+
+    vector<vector<HiringResult>> perform_experiment(int trials) {
         cout << "Performing experiment for " << trials << " trials\n";
         cout << "Please don't press anything ... \n";
         total_trials = trials;
-        map<int, double> results;
-        for(int i = 0; i < 100; i++) {
-            results[i] = average_skill(generate_data_for(i, trials));
+        vector<vector<HiringResult>>result;
+        result.reserve(total);
+        for(int i = 0; i < total; i++) {
+            result.push_back(generate_data_for(i, trials));
         }
-        return results;
+        return result;
     }
 
-    int best_strategy(const map<int, double>& results) {
-        auto it = results.begin();
-        double max_value = it->second;
-        int corresponding_key = it->first;
-        it++;
-        for(; it != results.end(); ++it){
-            if(it->second > max_value) {
-                max_value = it->second;
-                corresponding_key = it->first;
+    vector<Printer> calculate_results(vector<vector<HiringResult>>& result) {
+        vector<Printer> pages(total); 
+
+        for(int i = 0; i < total; i++) {
+            (pages.at(i)).rejections = i;
+            (pages.at(i)).final_avg_skill = average_skill(result.at(i));
+            (pages.at(i)).final_prob = prob_hired_best(result.at(i)); 
+            (pages.at(i)).final_avg_rank = average_rank(result.at(i));
+        }
+        return pages;
+    }
+
+    int max_prob_index(vector<Printer>& pages) {
+        double local_max = (pages.at(0)).final_prob;
+        int index = (pages.at(0)).rejections;
+        for(int i = 1; i < pages.size(); i++){
+            if((pages.at(i)).final_prob > local_max) {
+                local_max = (pages.at(i)).final_prob;
+                index = (pages.at(i)).rejections;
             }
         }
-        return corresponding_key;
+        return index;
     }
 
-    void print_results(const map<int, double>& results) {
-        int successful_key = best_strategy(results);
-        double max_skill = results.at(successful_key);
-        cout << "-------------------------------\n";
-        cout << "Results for " << total_trials << " trials for each strategy: \n\n";
-        cout << "Most successful strategy was for " << successful_key << " rejections\n";
-        cout << "Average Skill level of most successful hiring strategy is " << max_skill * 100 << "\%\n"; 
-        ask_for_histogram(results);
-        cout << "-------------------------------\n";
-        cout << "Simulation complete.\n";
-        cout << "-------------------------------\n";
+    int max_avg_skill_index(vector<Printer>& pages) {
+        double local_max = (pages.at(0)).final_avg_skill;
+        int index = (pages.at(0)).rejections;
+        for(int i = 1; i < pages.size(); i++){
+            if((pages.at(i)).final_avg_skill > local_max) {
+                local_max = (pages.at(i)).final_avg_skill;
+                index = (pages.at(i)).rejections;
+            }
+        }
+        return index;
     }
 
-    void draw_histogram(const map<int, double>& results) {
+    void draw_histogram(vector<Printer>& pages, int choice) {
         const int bar_width = 100; // Width of the histogram bars
-        cout << "Histogram of results:\n";
-        for (const auto& pair : results) {
-            std::cout << std::setw(3) << pair.first << " | " << std::string(pair.second * bar_width, '*') << " (" << pair.second * bar_width << " \%\n";
+        cout << "\nHistogram of results:\n";
+        if(choice == 0) {
+            for(int i = 0; i < pages.size(); i++) {
+                cout << std::setw(3) << (pages.at(i)).rejections << " | " << std::string((pages.at(i)).final_prob * bar_width, '*') << " (" << (pages.at(i)).final_prob * bar_width << " \%) [Average rank = " << (pages.at(i)).final_avg_rank << "/100 ]\n";
+            }
+        }
+        else {
+            for(int i = 0; i < pages.size(); i++) {
+                cout << std::setw(3) << (pages.at(i)).rejections << " | " << std::string((pages.at(i)).final_avg_skill * bar_width, '*') << " (" << (pages.at(i)).final_avg_skill * bar_width << " \%) \n";
+            }
         }
     }
 
-    void ask_for_histogram(const map<int, double>& results) {
+    void ask_for_histogram(vector<Printer>& pages, int c = 0) {
         cout << "-------------------------------\n";
         cout << "Would you like to see a histogram of the results? (y/n): ";
         char choice;
         std::cin >> choice;
         if (choice == 'y' || choice == 'Y') {
-            draw_histogram(results);
+            draw_histogram(pages, c);
         }
+    }
+
+
+    void print_results(vector<vector<HiringResult>>& result) {
+        vector<Printer> pages = calculate_results(result);
+        int index1 = max_prob_index(pages);
+        cout << "-------------------------------\n";
+        cout << "Results for " << total_trials << " trials for each strategy:\n\n";
+        cout << "-------------------------------\n";
+        cout << "Requirement: Hire the Best candidate from the " << total << " total candidates\n";
+        cout << "Solution: If best candidate hired, the experiment was successful\n\n";
+        cout << "Number of rejections that results in the highest probability of hiring the best candidate is " << index1 << endl;
+        cout << "Probability of successful drive being: " << (pages.at(index1)).final_prob * 100 << "\%\n";
+        cout << "Average rank hired when most successful strategy applied: " << (pages.at(index1)).final_avg_rank << " out of 100\n";
+        cout << "-------------------------------\n\n";
+        ask_for_histogram(pages, 0); // for strategy 1
+        int index2 = max_avg_skill_index(pages);
+        cout << "\nRequirement: Overall average hiring should result in best average skill percentile of hired candidates\n";
+        cout << "Solution: Average skill level of all " << total << " candidates hired through a strategy is considered\n\n";
+        cout << "Number of rejections that resulted in the highest average skill of candidate: " << index2 << " rejections\n";
+        cout << "Average skill level of hired candidate acheived: " << (pages.at(index2)).final_avg_skill * 100 << "\%\n";
+        ask_for_histogram(pages, 1); // for strategy 2
+        cout << "-------------------------------\n";
+        cout << "Simulation complete.\n";
+        cout << "-------------------------------\n";
     }
 
 };
 
 int main() {
-    constexpr int trials = 10000; 
+    constexpr int trials = 50000; 
     Secretary hr;
     auto results = hr.perform_experiment(trials);
 
